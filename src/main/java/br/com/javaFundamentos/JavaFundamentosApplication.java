d0f384a555bc786fbfa3825caef70b7ae735108f
Cláudio Leal <claudiolealti@gmail.com>
2020-11-18 11:08:04 -0300

    Spring Boot Project com dependências: JPA - PostgreSQL - Swagger

---
+++ src/main/java/br/com/javaFundamentos/JavaFundamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.javaFundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javaFundamentos/JavaFundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javaFundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
